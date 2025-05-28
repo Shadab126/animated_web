376dd32ddce00dca4c268bdb56c00e28f902e9d5
Md Junaid Ansari <75010559+Shadab126@users.noreply.github.com>
2025-05-29 00:43:12 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
-import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+import { FaLinkedin, FaInstagram, FaFacebook, FaTwitter, FaGithub } from "react-icons/fa";
 
 const socialLinks = [
-  { href: "https://discord.com", icon: <FaDiscord /> },
-  { href: "https://twitter.com", icon: <FaTwitter /> },
-  { href: "https://youtube.com", icon: <FaYoutube /> },
-  { href: "https://medium.com", icon: <FaMedium /> },
+  { href: "https://www.linkedin.com/in/shadab-ansari-6a86a7194/", icon: <FaLinkedin /> },
+  { href: "https://www.instagram.com/shadabansari33366/", icon: <FaInstagram /> },
+  { href: "https://www.facebook.com/sadab.afroz/", icon: <FaFacebook /> },
+  { href: "https://x.com/ShadabAnsarii23?t=kjFOrTrK8n6VkfA7Gz0N1g&s=09", icon: <FaTwitter /> },
+  { href: "https://github.com/Shadab126", icon: <FaGithub /> },
 ];
 
 const Footer = () => {
@@ -12,10 +13,10 @@ const Footer = () => {
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          ©Nova 2024. All rights reserved
+          ©Shadab Ansari 2025. All rights reserved
         </p>
 
-        <div className="flex justify-center gap-4  md:justify-start">
+        <div className="flex justify-center gap-4 md:justify-start">
           {socialLinks.map((link, index) => (
             <a
               key={index}
@@ -40,4 +41,4 @@ const Footer = () => {
   );
 };
 
-export default Footer;  
+export default Footer;
